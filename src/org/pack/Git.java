1892fe8289b1b3f74ff7e756eccff6eb385aad12
Dinesh <dnshkarthi726@gmail.com>
2021-03-19 06:08:55 +0530

    git project has commited

---
+++ src/org/pack/Git.java
@@ -0,0 +1,15 @@
+package org.pack;
+
+public class Git {
+	
+		public static void main(String[] args) {
+			System.out.println("java sample code");
+		}
+
+	public void email()
+	{
+		System.out.println("dnsh@gmail.com");
+	}
+
+
+}
